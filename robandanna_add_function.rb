795e56c9c15d1346986e055433ec50ba9b91004d
anna.kiuber <anna.kiuber@gmail.com>
2018-03-21 14:32:03 -0400

    division, and multiply test

--- robandanna_add_function.rb
+++ robandanna_add_function.rb
@@ -3,3 +3,11 @@ def adding(num1, num2)
 	a << num1 << num2
 	a.sum
 end
+
+def subtract(num1,num2)
+	num1 - num2 
+end
+
+def miltiply(num1,num2)
+	num1*num2
+end
--- robandanna_add_function_test.rb
+++ robandanna_add_function_test.rb
@@ -18,4 +18,28 @@ class Addcalctest < Minitest::Test
 	def test_2_and_3_euals_5
 		assert_equal(5,adding(2,3))
 	end
+
+	def test_subtract5_3_equals2
+		assert_equal(2,subtract(5,3))
+	end
+
+	def test_subtract7_10_equals3
+		assert_equal(3,subtract(10,7))
+	end
+
+	def testsubtract8_16_equals8
+		assert_equal(8,subtract(16,8))
+	end
+
+	def testmultiply8_8_equals64
+		assert_equal(64, multiply(8,8))
+	end
+
+	def testmultiply2_8_equals16
+		assert_equal(16, multiply(2,8))
+	end
+
+	def testmultiply4_4_equals16
+		assert_equal(16, multiply(4,4))
+	end
 end
