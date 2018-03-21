ec3a5545b87f86120c78d5193da32f6ad3c05963
anna.kiuber <anna.kiuber@gmail.com>
2018-03-21 14:01:08 -0400

    addtestdone

---
+++ robandanna_add_function.rb
@@ -0,0 +1,5 @@
+def adding(num1, num2)
+	a=[]
+	a << num1 << num2
+	a.sum
+end
--- robandanna_add_function_test.rb
+++ robandanna_add_function_test.rb
@@ -4,6 +4,18 @@ require_relative "robandanna_add_function.rb"
 class Addcalctest < Minitest::Test
 
 	 def test_1_equals_1
-		assert_equal(1,1)
+		assert_equal(1,1)#first one is what we are expecting second one we are checking
+	end
+
+	def test_1and1_equals_2
+		assert_equal(2,adding(1,1))
+	end
+
+	def test_1and3euals_4
+		assert_equal(4,adding(1,3))
+	end
+
+	def test_2_and_3_euals_5
+		assert_equal(5,adding(2,3))
 	end
 end
